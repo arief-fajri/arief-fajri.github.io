a7f74d63b86685a2856026add3ff8d37cb201e4e
arief-fajri <arief.fajri191@gmail.com>
2021-10-29 19:10:30 +0700

    make navbar

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,38 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import {
+    createRouter,
+    createWebHistory
+} from 'vue-router'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+import Home from '../views/Home.vue'
+import Expertise from '../views/Expertise.vue'
+import Work from '../views/Work.vue'
+import Contact from '../views/Contact.vue'
+
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/expertise',
+        name: 'Expertise',
+        component: Expertise
+    },
+    {
+        path: '/work',
+        name: 'Work',
+        component: Work
+    },
+    {
+        path: '/contact',
+        name: 'Contact',
+        component: Contact
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
